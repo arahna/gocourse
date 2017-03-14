30433bd007f0cdeac75b65029c37e9940ebee533
Julia Nikolaeva <julia.nikolaeva@ispringsolutions.com>
2017-03-14 15:45:15 +0300

    [Lab 1.1] Корректная обработка окончаний для чисел > 100, добавлен тест

--- lab1/lab1.1/battles.go
+++ lab1/lab1.1/battles.go
@@ -7,25 +7,25 @@ func main() {
 	for i := 99; i > 1; i-- {
 		ending1 := getEnding(i)
 		ending2 := getEnding(i - 1)
-		fmt.Printf("%v бутыл%v пива на стене\n%v бутыл%v пива!\nВозьми одну, пусти по кругу\n%v бутыл%v пива на стене!\n\n", i, ending1, i, ending1, i - 1, ending2)
+		fmt.Printf("%v бутыл%v пива на стене\n%v бутыл%v пива!\nВозьми одну, пусти по кругу\n%v бутыл%v пива на стене!\n\n", i, ending1, i, ending1, i-1, ending2)
 	}
 
 	fmt.Print("Нет бутылок пива на стене!\nНет бутылок пива!\nПойди в магазин и купи ещё,\n99 бутылок пива на стене!")
 }
 
 func getEnding(count int) string {
-	if count >= 5 && count <= 20 {
+	reminder1 := count % 100
+	if reminder1 >= 11 && reminder1 <= 15 {
 		return "ок"
 	}
 
-	reminder := count%10
-	if reminder == 1 {
+	reminder2 := count % 10
+	if reminder2 == 1 {
 		return "ка"
 	}
-	if reminder >= 2 && reminder <= 4 {
+	if reminder2 >= 2 && reminder2 <= 4 {
 		return "ки"
 	}
 
 	return "ок"
 }
-
